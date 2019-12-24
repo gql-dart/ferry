a1988c7c3d37a17e5a20351fccf07953f57cb8b4
Sat Mandir Khalsa <smkhalsa@Sats-MacBook-Pro.local>
2019-12-23 18:25:28 -0800

    basic refetch functionality

--- example/bin/main.dart
+++ example/bin/main.dart
@@ -1,17 +1,36 @@
-import '../generated/graphql_api.dart';
+import 'package:gql_http_link/gql_http_link.dart';
+
+import '../lib/graphql_api.dart';
 import '../../lib/src/client/client.dart';
 
 main() {
   () async {
-    final mutation = AddSongQuery();
+    final link = HttpLink('https://api.kundalini.dev');
 
-    final client = Client();
+    final client = Client(link: link);
 
     final queryRef = client.query(
-        SongsQuery(variables: SongsArguments(first: 10)),
-        QueryOptions(pollInterval: Duration(seconds: 1)));
+        SongsQuery(variables: SongsArguments(first: 10, offset: 0)),
+        pollInterval: Duration(seconds: 1));
 
-    final result = await queryRef.future;
+    queryRef.stream.listen((response) {
+      print(response.data.Song.length);
+    });
+
+    await Future.delayed(Duration(seconds: 3));
+
+    final refetchResult = await queryRef.refetch(
+        query: SongsQuery(variables: SongsArguments(first: 10, offset: 10)));
+
+    final refetchMergeResult = await queryRef.refetch(
+        query: SongsQuery(variables: SongsArguments(first: 15, offset: 20)),
+        updateResult: (original, updated) {
+          original.Song = [...original.Song, ...updated.Song];
+          return original;
+        });
+    print('complete');
+
+    // final mutationRef = client.mutation(AddSongQuery());
 
     // cache.root.writeQuery(
     //     query: query.document, variables: query.getVariablesMap(), data: data);
--- lib/src/client/client.dart
+++ lib/src/client/client.dart
@@ -1,59 +1,147 @@
+import 'package:meta/meta.dart';
 import 'dart:async';
-import 'package:gql/ast.dart';
+import 'package:gql/execution.dart';
+import 'package:gql_link/gql_link.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:json_annotation/json_annotation.dart';
-import 'package:artemis/schema/graphql_query.dart';
-import 'package:artemis/schema/graphql_response.dart';
+import 'package:uuid/uuid.dart';
+import 'package:artemis/artemis.dart';
+
+final uuid = Uuid();
 
 class MutationRef<T, U extends JsonSerializable> {
-  Future<GraphQLResponse<T>> run;
+  Future<GraphQLResponse<T, U>> run;
 }
 
 class QueryRef<T, U extends JsonSerializable> {
-  Stream<GraphQLResponse<T>> get stream {}
-  Future<GraphQLResponse<T>> get future {
-    return stream.first;
+  final String id = uuid.v4();
+  final Client _client;
+  final GraphQLQuery<T, U> _initialQuery;
+  final Duration pollingDuration;
+
+  StreamController<QueryEvent<T, U>> _controller;
+  Stream<GraphQLResponse<T, U>> stream;
+
+  QueryRef(Client client, GraphQLQuery<T, U> query, {this.pollingDuration})
+      : _client = client,
+        _initialQuery = query,
+        super() {
+    /// Initialize the StreamController
+    ///
+    /// the onCancel callback calls dispose to close the controller when there
+    /// are no longer any active listeners
+    _controller = StreamController(onListen: _onListen);
+
+    /// Initialize the stream
+    stream = _initStream();
   }
 
-  Future<GraphQLResponse<T>> refetch([U variables]) {}
-  Future<GraphQLResponse<T>> fetchMore(
-      {DocumentNode document,
-      U variables,
-      T Function(T previousResult, T result) updateQuery}) {}
+  void _onListen() {
+    _controller.add(QueryEvent<T, U>(queryId: this.id, query: _initialQuery));
+  }
+
+  /// Creates the stream
+  Stream<GraphQLResponse<T, U>> _initStream() {
+    final responseStream =
+        _controller.stream.switchMap((queryEvent) => _client.link
+            .request(Request(
+              operation: Operation(
+                document: queryEvent.query.document,
+                operationName: queryEvent.query.operationName,
+                variables: queryEvent.query.getVariablesMap(),
+              ),
+            ))
+            .map((response) => GraphQLResponse<T, U>(
+                  triggeringEvent: queryEvent,
+                  data: response.data == null
+                      ? null
+                      : queryEvent.query.parse(response.data),
+                  errors: response.errors,
+                )));
+
+    final updatedResultStream = responseStream
+        .startWith(null)
+        .shareReplay(maxSize: 1)
+        .pairwise()
+        .map((results) {
+      final previousResult = results.elementAt(0);
+      final result = results.elementAt(1);
+      if (result.triggeringEvent.updateResult != null) {
+        return GraphQLResponse(
+            data: result.triggeringEvent
+                .updateResult(previousResult.data, result.data),
+            errors: result.errors,
+            triggeringEvent: result.triggeringEvent);
+      } else {
+        return result;
+      }
+    });
+
+    return updatedResultStream;
+  }
+
+  Future<GraphQLResponse<T, U>> refetch(
+      {GraphQLQuery<T, U> query,
+      T Function(T previousResult, T result) updateResult}) {
+    final queryEvent = QueryEvent<T, U>(
+        queryId: this.id,
+        query: query ?? _initialQuery,
+        updateResult: updateResult);
+
+    // TODO: is there a better way to immediately add the queryEvent than using a future?
+    Future.delayed((Duration.zero)).then((_) => _controller.add(queryEvent));
+
+    return stream
+        .firstWhere((response) => response.triggeringEvent == queryEvent);
+  }
+
   void startPolling(Duration pollInterval) {}
   void stopPolling() {}
 }
 
-class QueryOptions {
-  final Duration pollInterval;
+/// Encapsulates a GraphQL query/mutation response, with typed
+/// input and responses, and errors.
+@immutable
+class GraphQLResponse<T, U extends JsonSerializable> {
+  /// The event that resulted in this response
+  final QueryEvent<T, U> triggeringEvent;
 
-  QueryOptions({this.pollInterval});
+  /// The typed data of this response.
+  final T data;
+
+  /// The list of errors in this response.
+  final List<GraphQLError> errors;
+
+  /// If this response has any error.
+  bool get hasErrors => errors != null && errors.isNotEmpty;
+
+  /// Instantiates a GraphQL response.
+  const GraphQLResponse({
+    @required this.triggeringEvent,
+    this.data,
+    this.errors,
+  });
 }
 
-class ClientOperation {
-  final String id;
-  final GraphQLQuery query;
+@immutable
+class QueryEvent<T, U extends JsonSerializable> {
+  final String queryId;
+  final GraphQLQuery<T, U> query;
+  final T Function(T previousResult, T result) updateResult;
 
-  ClientOperation({this.id, this.query});
+  const QueryEvent(
+      {@required this.queryId, @required this.query, this.updateResult});
 }
 
 class Client {
-  final PublishSubject<ClientOperation> _operationStream = PublishSubject();
+  final Link link;
+  final StreamController<QueryEvent> _queryEventStream = PublishSubject();
 
-  // TODO: use UUID - counter won't work offline
-  int _queryIdCounter = 1;
-  String _generateId() {
-    return (_queryIdCounter++).toString();
-  }
+  Client({@required this.link});
 
   QueryRef<T, U> query<T, U extends JsonSerializable>(GraphQLQuery<T, U> query,
-      [QueryOptions options]) {
-    final id = _generateId();
-    // TODO: create the stream
-    final operation = _operationStream.where((operation) => operation.id == id);
-    _operationStream.add(ClientOperation(id: id, query: query));
-    return operation;
-    return QueryRef<T, U>();
+      {Duration pollInterval}) {
+    return QueryRef<T, U>(this, query);
   }
 
   MutationRef<T, U> mutation<T, U extends JsonSerializable>(
