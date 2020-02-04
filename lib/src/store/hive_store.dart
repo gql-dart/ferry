f54488593ea9c34863cab1e05c7ef2ffdd6e5cf5
smkhalsa <smkhalsa@gmail.com>
2020-02-03 17:13:43 -0800

    fix hive store

--- lib/src/store/hive_store.dart
+++ lib/src/store/hive_store.dart
@@ -9,8 +9,17 @@ class HiveStore extends Store {
   final Box box;
 
   HiveStore(this.box) {
-    _controller.add(Map.from(box.toMap()));
+    _controller.add(_recast(box.toMap()));
   }
+
+  // Workaround for correct typecasting
+  Map<String, Map<String, dynamic>> _recast(Map<dynamic, dynamic> data) =>
+      data.map<String, Map<String, dynamic>>(
+        (key, value) => MapEntry(
+          key,
+          Map.from(value),
+        ),
+      );
 
   @override
   Stream<Map<String, Map<String, dynamic>>> watch() {
@@ -19,29 +28,31 @@ class HiveStore extends Store {
 
   @override
   Map<String, dynamic> get(String dataId) {
-    return Map.from(box.get(dataId));
+    final result = box.get(dataId);
+    if (result == null) return null;
+    return Map.from(result);
   }
 
   @override
   void put(String dataId, Map<String, dynamic> value) {
     box.put(dataId, value);
-    _controller.add(Map.from(box.toMap()));
+    _controller.add(_recast(box.toMap()));
   }
 
   @override
   void putAll(Map<String, Map<String, dynamic>> data) {
     box.putAll(data);
-    _controller.add(Map.from(box.toMap()));
+    _controller.add(_recast(box.toMap()));
   }
 
   @override
   void delete(String dataId) {
     box.delete(dataId);
-    _controller.add(Map.from(box.toMap()));
+    _controller.add(_recast(box.toMap()));
   }
 
   @override
   Map<String, Map<String, dynamic>> toMap() {
-    return Map.from(box.toMap());
+    return _recast(box.toMap());
   }
 }
