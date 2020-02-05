1abc37cfa6369a715373af09474dc73958a6d1b2
smkhalsa <smkhalsa@gmail.com>
2020-02-05 15:07:52 -0800

    remove test code

--- test/lib/graphql/songs_query.var.gql.dart
+++
@@ -1,6 +0,0 @@
-class SongsQueryVarBuilder {
-  final Map<String, dynamic> variables = <String, dynamic>{};
-
-  set first(int value) => variables['first'] = value;
-  set offset(int value) => variables['offset'] = value;
-}
