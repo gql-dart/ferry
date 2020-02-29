4c657c9edd77991089650b878c81932d594b9541
smkhalsa <smkhalsa@gmail.com>
2020-02-29 14:55:29 -0800

    update example

--- example/lib/src/graphql/all_pokemon.req.gql.dart
+++ example/lib/src/graphql/all_pokemon.req.gql.dart
@@ -3,29 +3,73 @@ import 'package:gql_example_flutter/src/graphql/all_pokemon.data.gql.dart'
     as _i2;
 import 'package:gql_example_flutter/src/graphql/all_pokemon.var.gql.dart'
     as _i3;
-import 'package:gql_example_flutter/src/graphql/all_pokemon.op.gql.dart' as _i4;
-import 'package:uuid/uuid.dart' as _i5;
+import 'package:gql_exec/gql_exec.dart' as _i4;
+import 'package:gql_example_flutter/src/graphql/all_pokemon.op.gql.dart' as _i5;
+import 'package:uuid/uuid.dart' as _i6;
 
 class AllPokemon extends _i1.QueryRequest<_i2.$AllPokemon> {
-  AllPokemon(
-      {_i3.AllPokemonVarBuilder Function(_i3.AllPokemonVarBuilder) buildVars,
+  factory AllPokemon(
+          {_i3.AllPokemonVarBuilder Function(_i3.AllPokemonVarBuilder)
+              buildVars,
+          String queryId,
+          _i2.$AllPokemon Function(_i2.$AllPokemon, _i2.$AllPokemon)
+              updateResult,
+          Map<String, dynamic> optimisticResponse,
+          updateCacheHandlerKey,
+          _i4.Context context,
+          _i1.FetchPolicy fetchPolicy}) =>
+      AllPokemon._(
+          operation: _i5.AllPokemon,
+          variables: buildVars != null
+              ? buildVars(_i3.AllPokemonVarBuilder()).variables
+              : const {},
+          context: context != null ? context : const _i4.Context(),
+          queryId: queryId != null ? queryId : _i6.Uuid().v4(),
+          updateResult: updateResult,
+          optimisticResponse: optimisticResponse,
+          updateCacheHandlerKey: updateCacheHandlerKey,
+          fetchPolicy: fetchPolicy);
+
+  AllPokemon._(
+      {_i4.Operation operation,
+      Map<String, dynamic> variables,
+      _i4.Context context,
       String queryId,
       _i2.$AllPokemon Function(_i2.$AllPokemon, _i2.$AllPokemon) updateResult,
       Map<String, dynamic> optimisticResponse,
-      updateCacheHandlerKey,
-      Map<String, dynamic> context,
+      String updateCacheHandlerKey,
       _i1.FetchPolicy fetchPolicy})
       : super(
-            operation: _i4.AllPokemon,
-            variables: buildVars != null
-                ? buildVars(_i3.AllPokemonVarBuilder()).variables
-                : {},
-            queryId: queryId != null ? queryId : _i5.Uuid().v4(),
+            operation: operation,
+            variables: variables,
+            context: context,
+            queryId: queryId,
             updateResult: updateResult,
             optimisticResponse: optimisticResponse,
             updateCacheHandlerKey: updateCacheHandlerKey,
-            context: context,
             fetchPolicy: fetchPolicy);
 
+  @override
   _i2.$AllPokemon parseData(Map<String, dynamic> json) => _i2.$AllPokemon(json);
+  @override
+  AllPokemon copyWith(
+          {_i4.Operation operation,
+          Map<String, dynamic> variables,
+          _i4.Context context,
+          String queryId,
+          _i2.$AllPokemon Function(_i2.$AllPokemon, _i2.$AllPokemon)
+              updateResult,
+          Map<String, dynamic> optimisticResponse,
+          String updateCacheHandlerKey,
+          _i1.FetchPolicy fetchPolicy}) =>
+      AllPokemon._(
+          operation: operation ?? this.operation,
+          variables: variables ?? this.variables,
+          context: context ?? this.context,
+          queryId: queryId ?? this.queryId,
+          updateResult: updateResult ?? this.updateResult,
+          optimisticResponse: optimisticResponse ?? this.optimisticResponse,
+          updateCacheHandlerKey:
+              updateCacheHandlerKey ?? this.updateCacheHandlerKey,
+          fetchPolicy: fetchPolicy ?? this.fetchPolicy);
 }
