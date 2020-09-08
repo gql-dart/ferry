ffb8bc78f4eafb2a26fe5daec2bd7cff0d5862fb
smkhalsa <smkhalsa@gmail.com>
2020-09-08 23:26:41 +0100

    fix normalize null error

--- normalize/lib/normalize.dart
+++ normalize/lib/normalize.dart
@@ -1,14 +1,8 @@
 export 'src/normalize_operation.dart';
 export 'src/denormalize_operation.dart';
-
 export 'src/denormalize_fragment.dart';
 export 'src/normalize_fragment.dart';
 
 export 'src/policies/field_policy.dart';
 export 'src/policies/type_policy.dart';
-export 'src/utils/add_typename_visitor.dart';
 export 'src/utils/exceptions.dart';
-export 'src/utils/get_operation_definition.dart';
-export 'src/utils/resolve_root_typename.dart';
-export 'src/utils/resolve_data_id.dart';
-export 'src/utils/field_key.dart';
---
+++ normalize/lib/src/utils/identify.dart
@@ -0,0 +1,17 @@
+import 'package:normalize/src/policies/type_policy.dart';
+import './resolve_data_id.dart';
+
+/// Returns the canonical ID for a given object or reference.
+String identify(
+  Map data, {
+  String referenceKey = '\$ref',
+  Map<String, TypePolicy> typePolicies,
+  DataIdResolver dataIdFromObject,
+}) =>
+    data.containsKey(referenceKey)
+        ? data
+        : resolveDataId(
+            data: data,
+            typePolicies: typePolicies,
+            dataIdFromObject: dataIdFromObject,
+          );
