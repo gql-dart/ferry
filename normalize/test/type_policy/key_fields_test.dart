a3afa090e794eb4bbf0cfd774c2675010de5ae2c
smkhalsa <smkhalsa@gmail.com>
2020-09-10 00:44:29 +0100

    add tests

--- normalize/test/type_policy/key_fields_test.dart
+++ normalize/test/type_policy/key_fields_test.dart
@@ -233,5 +233,64 @@ void main() {
         );
       });
     });
+
+    group('with empty keyFields', () {
+      final normalizedMap = {
+        'Query': {
+          '__typename': 'Query',
+          'posts': [
+            {
+              'id': '123',
+              '__typename': 'Post',
+              'author': {'\$ref': 'Author:1'},
+              'title': 'My awesome blog post',
+              'comments': [
+                {'\$ref': 'Comment:324'}
+              ]
+            }
+          ]
+        },
+        'Author:1': {'id': '1', '__typename': 'Author', 'name': 'Paul'},
+        'Comment:324': {
+          'id': '324',
+          '__typename': 'Comment',
+          'commenter': {'\$ref': 'Author:2'}
+        },
+        'Author:2': {'id': '2', '__typename': 'Author', 'name': 'Nicole'}
+      };
+
+      final typePolicies = {
+        'Post': TypePolicy(
+          keyFields: {},
+        )
+      };
+
+      test('Produces correct normalized object', () {
+        final normalizedResult = {};
+        normalizeOperation(
+          read: (dataId) => normalizedResult[dataId],
+          write: (dataId, value) => normalizedResult[dataId] = value,
+          document: query,
+          data: sharedResponse,
+          typePolicies: typePolicies,
+        );
+
+        expect(
+          normalizedResult,
+          equals(normalizedMap),
+        );
+      });
+
+      test('Produces correct nested data object', () {
+        expect(
+          denormalizeOperation(
+            document: query,
+            read: (dataId) => normalizedMap[dataId],
+            typePolicies: typePolicies,
+          ),
+          equals(sharedResponse),
+        );
+      });
+    });
   });
 }
