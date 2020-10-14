907d0b8472ca0ea0df195af409e6160fc085df99
micimize <rosenthalm93@gmail.com>
2020-10-14 13:08:37 -0500

    clarify data id resolution docstring

--- normalize/lib/src/utils/resolve_data_id.dart
+++ normalize/lib/src/utils/resolve_data_id.dart
@@ -11,8 +11,8 @@ typedef DataIdResolver = String Function(Map<String, dynamic> object);
 /// Returns a unique ID to use to reference this normalized object.
 ///
 /// First checks if a [TypePolicy] exists for the given type. Next,
-/// calls the [dataIdFromObject] function if one is specified. Finally,
-/// falls back to the 'id' or '_id' field, respectively.
+/// calls the [dataIdFromObject] function if one is specified.
+/// If none is provided, falls back to the 'id' or '_id' field, respectively.
 ///
 /// Returns [null] if this type should not be normalized.
 String resolveDataId({
