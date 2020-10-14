ea8603038919b4c300fa791c2e4bb37b68a63bdc
micimize <rosenthalm93@gmail.com>
2020-10-14 13:06:43 -0500

    add path to PartialDataException

--- normalize/lib/src/utils/exceptions.dart
+++ normalize/lib/src/utils/exceptions.dart
@@ -1,3 +1,19 @@
-class PartialDataException implements Exception {}
+import 'package:meta/meta.dart';
+import 'package:normalize/utils.dart';
+
+/// Exception occurring when structurally valid data cannot be resolved
+/// for an expected field.
+@immutable
+class PartialDataException implements Exception {
+  PartialDataException({@required this.path});
+
+  /// Path to the first unfound subfield.
+  ///
+  /// Is a list of field names stringified with [FieldKey]
+  final List<String> path;
+
+  @override
+  String toString() => 'PartialDataException(path: ${path.join(".")})';
+}
 
 class MissingKeyFieldException implements Exception {}
