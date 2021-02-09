8b2bb221a1e5b2cf6f44202ad5c0480d3fc45634
smkhalsa <smkhalsa@gmail.com>
2021-02-09 19:11:00 +0100

    add watchFragment

---
+++ ferry_cache/lib/src/utils/data_for_id_stream.dart
@@ -0,0 +1,21 @@
+import 'package:ferry_store/ferry_store.dart';
+import 'package:rxdart/rxdart.dart';
+
+import 'package:ferry_exec/ferry_exec.dart';
+
+/// Returns the normalized data for the given [dataId], optionally merging optimistic data.
+Stream<Map<String, dynamic>> dataForIdStream(
+  String dataId,
+  Store store,
+  bool optimistic,
+  Stream<Map<OperationRequest, Map<String, Map<String, dynamic>>>>
+      optimisticPatchesStream,
+  Map<String, dynamic> Function(String dataId) optimisticReader,
+) =>
+    optimistic
+        ? CombineLatestStream.combine2(
+            store.watch(dataId),
+            optimisticPatchesStream,
+            (_, __) => optimisticReader(dataId),
+          )
+        : store.watch(dataId);
---
+++ ferry_cache/lib/src/utils/operation_root_data.dart
@@ -0,0 +1,24 @@
+import 'package:normalize/policies.dart';
+import 'package:normalize/utils.dart';
+import 'package:normalize/normalize.dart';
+
+import 'package:ferry_exec/ferry_exec.dart';
+
+/// Returns a subset of the [data] with only the fields that this operation uses.
+Map<String, dynamic> operationRootData<TData, TVars>(
+  Map<String, dynamic> data,
+  OperationRequest<TData, TVars> request,
+  Map<String, TypePolicy> typePolicies,
+) {
+  final fieldNames = operationFieldNames(
+    request.operation.document,
+    request.operation.operationName,
+    (request.vars as dynamic).toJson(),
+    typePolicies,
+  );
+  return {
+    for (var fieldName in fieldNames)
+      if (data != null && data.containsKey(fieldName))
+        fieldName: data[fieldName]
+  };
+}
