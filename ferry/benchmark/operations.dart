f90cab00bc82a91aaa6c51b85498fbf6c7a2e148
Sat Mandir S. Khalsa <smkhalsa@gmail.com>
2021-09-01 16:20:53 +0200

    feat: add benchmarks (#256)

---
+++ ferry/benchmark/operations.dart
@@ -0,0 +1,41 @@
+import 'package:ferry/ferry.dart';
+import 'package:gql_exec/gql_exec.dart';
+import 'package:gql_link/gql_link.dart';
+import 'package:benchmarking/benchmarking.dart';
+
+import 'package:ferry_test_graphql/queries/variables/human_with_args.req.gql.dart';
+import 'package:ferry_test_graphql/queries/variables/human_with_args.data.gql.dart';
+
+const _numItems = 1000;
+
+void main() async {
+  final result = await asyncBenchmark('Query', query);
+  result.report(units: _numItems);
+}
+
+Future<void> query() async {
+  final client = Client(link: AutoResponderLink());
+  final numbers = List.generate(_numItems, (i) => i);
+
+  for (final num in numbers) {
+    final req = GHumanWithArgsReq((b) => b..vars.id = num.toString());
+    await client.request(req).first;
+  }
+}
+
+class AutoResponderLink extends Link {
+  @override
+  Stream<Response> request(
+    Request req, [
+    forward,
+  ]) =>
+      Stream.value(
+        Response(
+          data: GHumanWithArgsData(
+            (b) => b
+              ..human.id = req.variables['id']
+              ..human.name = 'Luke',
+          ).toJson(),
+        ),
+      );
+}
