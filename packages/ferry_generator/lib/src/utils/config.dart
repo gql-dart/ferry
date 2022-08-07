22e0d2548ccf3f9c4ea93770956a44b65a46d4da
Martin Kamleithner <martin+kamleithner@diagnosia.com>
2022-08-08 00:41:22 +0200

    chore: dart format

--- packages/ferry_generator/lib/src/utils/config.dart
+++ packages/ferry_generator/lib/src/utils/config.dart
@@ -23,7 +23,8 @@ class BuilderConfig {
       : schemaId = AssetId.parse(config['schema'] as String),
         shouldAddTypenames = config['add_typenames'] ?? true,
         typeOverrides = _getTypeOverrides(config['type_overrides']),
-        shouldGeneratePossibleTypes = config['generate_possible_types_map'] ?? true,
+        shouldGeneratePossibleTypes =
+            config['generate_possible_types_map'] ?? true,
         customSerializers = _getCustomSerializers(config['custom_serializers']),
         enumFallbackConfig = _getEnumFallbackConfig(config),
         outputDir = config['output_dir'] ?? '__generated__',
