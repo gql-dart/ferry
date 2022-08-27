8e96f0a38d445b9992fb8c51adad42237bdc41e9
Martin Kamleithner <martin+kamleithner@diagnosia.com>
2022-08-27 20:39:24 +0200

    chore(ferry_cache): more tests

--- packages/ferry_cache/test/watch_test.dart
+++ packages/ferry_cache/test/watch_test.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:ferry_test_graphql/queries/__generated__/review_by_id.data.gql.dart';
+import 'package:ferry_test_graphql/queries/__generated__/review_by_id.req.gql.dart';
 import 'package:test/test.dart';
 import 'package:ferry_cache/ferry_cache.dart';
 
@@ -15,8 +17,19 @@ final review = GReviewsData_reviews(
     ..commentary = 'Pretty Good',
 );
 
+final review2 = GReviewsData_reviews(
+  (b) => b
+    ..id = '456'
+    ..stars = 3
+    ..commentary = 'Okay',
+);
+
 final reviewsData = GReviewsData(
   (b) => b..reviews.add(review),
+);
+
+final reviewsData2 = GReviewsData(
+  (b) => b..reviews.addAll([review, review2]),
 );
 
 void main() {
@@ -27,7 +40,7 @@ void main() {
       cache.dispose();
     });
 
-    test('can return data', () {
+    test('can return data with', () {
       final cache = Cache();
 
       cache.writeQuery(reviewsReq, reviewsData);
@@ -61,6 +74,102 @@ void main() {
       await Future.delayed(Duration.zero);
 
       cache.writeQuery(reviewsReq, nextData);
+
+      await Future.delayed(Duration.zero);
+
+      await cache.dispose();
+    });
+
+    test('can receive updates to data when starting with empty data', () async {
+      final cache = Cache();
+
+      final nextData =
+          reviewsData.rebuild((b) => b.reviews.add(review.rebuild((b) => b..id = '456')));
+
+      expect(
+        cache.watchQuery(reviewsReq),
+        emitsInOrder([
+          isNull,
+          reviewsData,
+          nextData,
+          emitsDone,
+        ]),
+      );
+
+      await Future.delayed(Duration.zero);
+
+      cache.writeQuery(reviewsReq, reviewsData);
+
+      await Future.delayed(Duration.zero);
+
+      cache.writeQuery(reviewsReq, nextData);
+
+      await Future.delayed(Duration.zero);
+
+      await cache.dispose();
+    });
+
+    test('can resume watch after reading corrupt data from cache', () async {
+      final cache = Cache();
+
+      cache.writeQuery(reviewsReq, reviewsData);
+
+      final dataId = cache.identify(reviewsData.reviews!.first)!;
+
+      final data = cache.store.get(dataId);
+
+      //corrupt the data -> write String where an int should be
+      data!['stars'] = '100';
+      cache.store.put(dataId, data);
+
+      final nextData =
+          reviewsData.rebuild((b) => b.reviews.add(review.rebuild((b) => b..id = '456')));
+
+      expect(
+          cache.watchQuery(reviewsReq),
+          emitsInOrder([
+            emitsError(isA<Error>()),
+            reviewsData,
+            nextData,
+            emitsDone,
+          ]));
+
+      await Future.delayed(Duration.zero);
+
+      cache.writeQuery(reviewsReq, reviewsData);
+
+      await Future.delayed(Duration.zero);
+
+      cache.writeQuery(reviewsReq, nextData);
+
+      await Future.delayed(Duration.zero);
+
+      await cache.dispose();
+    });
+
+    test('can receive updates when child objects are updated by other queries', () async {
+      final cache = Cache();
+      cache.writeQuery(reviewsReq, reviewsData2);
+
+      final updatedReview = reviewsData.reviews!.first.rebuild((b) => b.commentary = 'first');
+
+      expect(
+          cache.watchQuery(reviewsReq),
+          emitsInOrder([
+            reviewsData2,
+            reviewsData2.rebuild((b) => b..reviews[0] = updatedReview),
+            emitsDone,
+          ]));
+
+      await Future.delayed(Duration.zero);
+      cache.writeQuery(
+          GReviewsByIDReq((b) => b..vars.id = reviewsData.reviews!.first.id),
+          GReviewsByIDData((b) => b.review
+            ..id = updatedReview.id
+            ..stars = updatedReview.stars
+            ..createdAt = updatedReview.createdAt
+            ..seenOn = updatedReview.seenOn.toBuilder()
+            ..commentary = updatedReview.commentary));
 
       await Future.delayed(Duration.zero);
 
---
+++ packages/ferry_test_graphql/lib/queries/__generated__/review_by_id.ast.gql.dart
@@ -0,0 +1,67 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:gql/ast.dart' as _i1;
+
+const ReviewsByID = _i1.OperationDefinitionNode(
+    type: _i1.OperationType.query,
+    name: _i1.NameNode(value: 'ReviewsByID'),
+    variableDefinitions: [
+      _i1.VariableDefinitionNode(
+          variable: _i1.VariableNode(name: _i1.NameNode(value: 'id')),
+          type: _i1.NamedTypeNode(
+              name: _i1.NameNode(value: 'ID'), isNonNull: true),
+          defaultValue: _i1.DefaultValueNode(value: null),
+          directives: [])
+    ],
+    directives: [],
+    selectionSet: _i1.SelectionSetNode(selections: [
+      _i1.FieldNode(
+          name: _i1.NameNode(value: 'review'),
+          alias: null,
+          arguments: [
+            _i1.ArgumentNode(
+                name: _i1.NameNode(value: 'id'),
+                value: _i1.VariableNode(name: _i1.NameNode(value: 'id')))
+          ],
+          directives: [],
+          selectionSet: _i1.SelectionSetNode(selections: [
+            _i1.FieldNode(
+                name: _i1.NameNode(value: 'id'),
+                alias: null,
+                arguments: [],
+                directives: [],
+                selectionSet: null),
+            _i1.FieldNode(
+                name: _i1.NameNode(value: 'episode'),
+                alias: null,
+                arguments: [],
+                directives: [],
+                selectionSet: null),
+            _i1.FieldNode(
+                name: _i1.NameNode(value: 'stars'),
+                alias: null,
+                arguments: [],
+                directives: [],
+                selectionSet: null),
+            _i1.FieldNode(
+                name: _i1.NameNode(value: 'commentary'),
+                alias: null,
+                arguments: [],
+                directives: [],
+                selectionSet: null),
+            _i1.FieldNode(
+                name: _i1.NameNode(value: 'createdAt'),
+                alias: null,
+                arguments: [],
+                directives: [],
+                selectionSet: null),
+            _i1.FieldNode(
+                name: _i1.NameNode(value: 'seenOn'),
+                alias: null,
+                arguments: [],
+                directives: [],
+                selectionSet: null)
+          ]))
+    ]));
+const document = _i1.DocumentNode(definitions: [ReviewsByID]);
