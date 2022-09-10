1a100e0d89d41af11ba189960ba8dfdc47f6d99c
GP4cK <gautier.bayzelon@gmail.com>
2022-09-10 14:26:16 +0800

    fix(cache): Watch new items when added to the cache

--- packages/ferry_cache/lib/src/operation_data_change_stream.dart
+++ packages/ferry_cache/lib/src/operation_data_change_stream.dart
@@ -1,11 +1,12 @@
-import 'package:normalize/policies.dart';
-import 'package:normalize/utils.dart';
-import 'package:normalize/normalize.dart';
+import 'dart:async';
+
 import 'package:collection/collection.dart';
+import 'package:ferry_exec/ferry_exec.dart';
 import 'package:ferry_store/ferry_store.dart';
+import 'package:normalize/normalize.dart';
+import 'package:normalize/utils.dart';
 import 'package:rxdart/rxdart.dart';
 
-import 'package:ferry_exec/ferry_exec.dart';
 import './utils/data_for_id_stream.dart';
 import './utils/operation_root_data.dart';
 
@@ -31,67 +32,81 @@ Stream<Set<String>> operationDataChangeStream<TData, TVars>(
     typePolicies,
   );
 
-  final dataIds = <String>{};
+  final dataIdStreamController = StreamController<void>();
+  final result = dataIdStreamController.stream
+      .map((_) {
+        final dataIds = <String>{};
 
-  denormalizeOperation(
-      read: (dataId) {
-        dataIds.add(dataId);
-        return optimistic ? optimisticReader(dataId) : store.get(dataId);
-      },
-      document: request.operation.document,
-      operationName: request.operation.operationName,
-      // TODO: don't cast to dynamic
-      variables: (request.vars as dynamic)?.toJson(),
-      typePolicies: typePolicies,
-      addTypename: addTypename,
-      returnPartialData: true,
-      dataIdFromObject: dataIdFromObject,
-      possibleTypes: possibleTypes);
+        denormalizeOperation(
+          read: (dataId) {
+            dataIds.add(dataId);
+            return optimistic ? optimisticReader(dataId) : store.get(dataId);
+          },
+          document: request.operation.document,
+          operationName: request.operation.operationName,
+          // TODO: don't cast to dynamic
+          variables: (request.vars as dynamic)?.toJson(),
+          typePolicies: typePolicies,
+          addTypename: addTypename,
+          returnPartialData: true,
+          dataIdFromObject: dataIdFromObject,
+          possibleTypes: possibleTypes,
+        );
 
-  /// IDs that have changed
-  final changed = <String>{};
+        return dataIds;
+      })
+      .distinct()
+      .switchMap((dataIds) {
+        /// IDs that have changed
+        final changed = <String>{};
 
-  /// Streams for each dataId referenced in data, including the [rootTypename]
-  final streams = dataIds.map((dataId) {
-    var stream = dataForIdStream(
-      dataId,
-      store,
-      optimistic,
-      optimisticPatchesStream,
-      optimisticReader,
-    );
+        /// Streams for each dataId referenced in data, including the [rootTypename]
+        final streams = dataIds.map((dataId) {
+          var stream = dataForIdStream(
+            dataId,
+            store,
+            optimistic,
+            optimisticPatchesStream,
+            optimisticReader,
+          );
 
-    /// we only want to subscribe to changes on the operation fields, not the entire operation type)
-    if (dataId == rootTypename) {
-      stream = stream.map(
-        (data) => operationRootData(
-          data,
-          request,
-          typePolicies,
-          possibleTypes,
-        ),
-      );
-    }
+          /// we only want to subscribe to changes on the operation fields, not the entire operation type)
+          if (dataId == rootTypename) {
+            stream = stream.map(
+              (data) => operationRootData(
+                data,
+                request,
+                typePolicies,
+                possibleTypes,
+              ),
+            );
+          }
 
-    return stream
-        .distinct(
-          (prev, next) => const DeepCollectionEquality().equals(
-            prev,
-            next,
-          ),
+          return stream.distinct(
+            (prev, next) {
+              final areEqual = const MapEquality().equals(prev, next);
+              if (!areEqual) {
+                // Maybe a new element was added to an array,
+                // we need to recompute the dataIds
+                dataIdStreamController.add(null);
+              }
+              return areEqual;
+            },
+          ).doOnData((_) => changed.add(dataId));
+        });
+
+        return CombineLatestStream<Map<String, dynamic>?, Set<String>>(
+          streams,
+          (_) {
+            final result = {...changed};
+            changed.clear();
+            return result;
+          },
         )
-        .doOnData((_) => changed.add(dataId));
-  });
 
-  return CombineLatestStream<Map<String, dynamic>?, Set<String>>(
-    streams,
-    (_) {
-      final result = {...changed};
-      changed.clear();
-      return result;
-    },
-  )
-
-      /// Skip the first result since this returns the existsing data
-      .skip(1);
+            /// Skip the first result since this returns the existing data
+            .skip(1);
+      });
+  dataIdStreamController.add(null);
+  return result;
 }
