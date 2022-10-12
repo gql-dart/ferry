98cdfa6f88bc596200d694c211fb2b6cabdb1daa
Martin Kamleithner <martin.kamleithner@gmail.com>
2022-10-12 12:40:16 +0200

    Update packages/normalize/lib/src/utils/well_known_directives.dart
    Co-authored-by: Christian Budde Christensen <budde377@gmail.com>

--- packages/normalize/lib/src/utils/well_known_directives.dart
+++ packages/normalize/lib/src/utils/well_known_directives.dart
@@ -7,33 +7,13 @@ import 'package:gql/ast.dart';
 bool isSkipped(FieldNode node, Map<String, dynamic> variables) {
   final directives = node.directives;
 
-  if (directives.isEmpty) {
-    return false;
-  }
-
-  final DirectiveNode? skipOrIncludeDirective;
-  final bool isSkipDirective;
-
+ 
   final skipDirective = directives
-      .firstWhereOrNull((directive) => directive.name.value == 'skip');
-  if (skipDirective != null) {
-    skipOrIncludeDirective = skipDirective;
-    isSkipDirective = true;
-  } else {
-    final includeDirective = directives
-        .firstWhereOrNull((directive) => directive.name.value == 'include');
-    if (includeDirective != null) {
-      skipOrIncludeDirective = includeDirective;
-      isSkipDirective = false;
-    } else {
-      skipOrIncludeDirective = null;
-      isSkipDirective = false;
-    }
-  }
-
-  if (skipOrIncludeDirective == null) {
+      .firstWhereOrNull((directive) => const {'skip', 'include'}.contains( directive.name.value) );
+  if (skipDirective == null) {
     return false;
   }
+  final isSkipDirective = skipDirective.name.value === 'skip';
 
   final ifArgument = skipOrIncludeDirective.arguments
       .firstWhereOrNull((argument) => argument.name.value == 'if');
@@ -49,16 +29,15 @@ bool isSkipped(FieldNode node, Map<String, dynamic> variables) {
     return isSkipDirective ? ifValue.value : !ifValue.value;
   }
   // argument to if is a variable -> look up value of that variable in variables map
-  if (ifValue is VariableNode) {
-    final variableName = ifValue.name.value;
-    final variableValue = variables[variableName];
-    if (variableValue is bool) {
-      return isSkipDirective ? variableValue : !variableValue;
-    }
-
-    /// TODO: throw error if variable is not a boolean?
+  if (ifValue is! VariableNode) {
+    /// TODO: throw error if ifValue is neither a boolean literal nor a variable?
+    return false;
   }
-
-  /// TODO: throw error if ifValue is neither a boolean literal nor a variable?
-  return false;
+  final variableName = ifValue.name.value;
+  final variableValue = variables[variableName];
+  if (variableValue is! bool) {
+   /// TODO: throw error if variable is not a boolean?
+    return false;
+  }
+  return isSkipDirective ? variableValue : !variableValue;
 }
