174063bac7696a52243d8875738d7dcbf4d81631
Martin Kamleithner <martin.kamleithner@gmail.com>
2023-08-13 18:41:17 +0200

    chore: update codegen

--- packages/ferry_test_graphql2/lib/mutations/__generated__/create_review.data.gql.g.dart
+++ packages/ferry_test_graphql2/lib/mutations/__generated__/create_review.data.gql.g.dart
@@ -181,7 +181,11 @@ class _$GCreateReviewData extends GCreateReviewData {
 
   @override
   int get hashCode {
-    return $jf($jc($jc(0, G__typename.hashCode), createReview.hashCode));
+    var _$hash = 0;
+    _$hash = $jc(_$hash, G__typename.hashCode);
+    _$hash = $jc(_$hash, createReview.hashCode);
+    _$hash = $jf(_$hash);
+    return _$hash;
   }
 
   @override
@@ -312,12 +316,14 @@ class _$GCreateReviewData_createReview extends GCreateReviewData_createReview {
 
   @override
   int get hashCode {
-    return $jf($jc(
-        $jc(
-            $jc($jc($jc(0, G__typename.hashCode), id.hashCode),
-                episode.hashCode),
-            stars.hashCode),
-        commentary.hashCode));
+    var _$hash = 0;
+    _$hash = $jc(_$hash, G__typename.hashCode);
+    _$hash = $jc(_$hash, id.hashCode);
+    _$hash = $jc(_$hash, episode.hashCode);
+    _$hash = $jc(_$hash, stars.hashCode);
+    _$hash = $jc(_$hash, commentary.hashCode);
+    _$hash = $jf(_$hash);
+    return _$hash;
   }
 
   @override
@@ -405,4 +411,4 @@ class GCreateReviewData_createReviewBuilder
   }
 }
 
-// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,no_leading_underscores_for_local_identifiers,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new,unnecessary_lambdas
+// ignore_for_file: deprecated_member_use_from_same_package,type=lint
